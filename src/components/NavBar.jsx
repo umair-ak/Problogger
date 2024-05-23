f6a450f3025c2026db3afc9113a17571eb7a1cf6
Umair <uak1911@gmail.com>
2024-05-23 23:40:26 +0530

    Added the MUI theme to the NavBar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
 import "./navstyle.css"
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { blueGrey } from '@mui/material/colors';
+import { Link } from 'react-router-dom';
+import CssBaseline from '@mui/material/CssBaseline';
+
+
+const defaultTheme = createTheme({
+  palette: {
+  primary: {
+    main: blueGrey[500],
+  },
+  secondary: {
+    main: blueGrey[700],
+  },
+}});
 
 export default function NavBar() {
   return (
-    <div> <ul>
-    <li><a className="active" href="#home">Home</a></li>
-    <li><a href="#Newpost">New Post</a></li>
-    <li><a href="#contact">Contact</a></li>
-    <li><a href="#about">About</a></li>
-  </ul></div>
+    <ThemeProvider theme={defaultTheme}>
+    <CssBaseline />
+    <ul>
+    <li><Link to='/' className="active" href="#home">Home</Link></li>
+    <li><Link to='login' >New Post</Link></li>
+    <li><Link to="#">Contact</Link></li>
+    <li><Link to="#">About</Link></li>
+    </ul>
+   
+    </ThemeProvider>
     
   )
 }
