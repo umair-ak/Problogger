bb20c897451f10e39ad99cad84a5ba087b1faeaf
Umair <uak1911@gmail.com>
2024-05-23 23:13:36 +0530

    created a react router and changed the file structure

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/prop-types */
 import * as React from 'react';
 import NavBar from './NavBar';
+import { Outlet } from 'react-router-dom';
 
-export default function Base({children}) {
+export default function Base() {
   return (
     <div>
    <NavBar />
-    {children}
+    <Outlet />
     </div>
         );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import '@fontsource/inter';
 import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Base from './components/Base.jsx';
+import Home from './pages/Home.jsx';
+import Login from './pages/Login.jsx';
+import Signup from './pages/Signup.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Base />}>
+    <Route path='' element={<Home />} />
+    <Route path='login' element={<Login />} />
+    <Route path='signup' element={<Signup />} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
