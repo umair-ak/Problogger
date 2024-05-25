d7b0b8f2a9fce25dc388dc5bc8529071e3f399c5
Umair <uak1911@gmail.com>
2024-05-26 00:44:08 +0530

    Added private routes to prevent unauthorized users to access the dashboard

---
+++ src/components/User.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+
+export default function User() {
+  
+    // Create a login funciton that would check if the user is registered and logged in
+    let loggedin = false;
+
+    if(loggedin){ 
+        return <Outlet />;
+    }else{
+        console.log("user not logged in");
+        return <Navigate to={"/login"} />;
+    }
+
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import '@fontsource/inter';
 import './index.css'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { Route, RouterProvider, createBrowserRouter, Routes,createRoutesFromElements } from 'react-router-dom';
 import Base from './components/Base.jsx';
 import Home from './pages/Home.jsx';
 import Login from './pages/Login.jsx';
 import Signup from './pages/Signup.jsx'
+import User from './components/User.jsx';
+import Dashboard from './pages/Dashboard.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
+    
     <Route path='/' element={<Base />}>
-    <Route path='' element={<Home />} />
-    <Route path='login' element={<Login />} />
-    <Route path='signup' element={<Signup />} />
+      <Route index element={<Home />} />
+      <Route path='login' element={<Login />} />
+      <Route path='signup' element={<Signup />} />
+      
+        <Route path='user' element={<User />}>
+        <Route path='dashboard' element={<Dashboard />} />
+        </Route>
     </Route>
+        
   )
 )
 
