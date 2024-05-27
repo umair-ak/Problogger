d41fd9b1e1da4f23d6e902317972b6c0b1a0c2ad
Umair <uak1911@gmail.com>
2024-05-28 02:01:10 +0530

    Added router links to the login and signup page for the buttons on the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import GroupsIcon from '@mui/icons-material/Groups';
 import LightbulbIcon from '@mui/icons-material/Lightbulb';
 import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
@@ -11,12 +12,13 @@ export default function Home() {
     <div className='flex flex-wrap justify-evenly'>
     <div className='mt-5 pt-10 mb-20 content-center'>
     
-    <h2 className='text-8xl pb-5'>ProBlogger</h2>
+    <h2 className='sm:text-6xl md:text-7xl text-8xl pb-5'>ProBlogger</h2>
     <p className='text-3xl'>Connect through words.</p>
     
     {/*Buttons */}
     <div className='mt-5'>
-    <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-600 dark:text-white dark:border-gray-600 dark:hover:bg-white dark:hover:border-gray-600 dark:hover:text-gray-950 dark:focus:ring-gray-700">Register</button><button type="button" className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Login</button>
+    <Link to='/signup'>
+    <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-600 dark:text-white dark:border-gray-600 dark:hover:bg-white dark:hover:border-gray-600 dark:hover:text-gray-950 dark:focus:ring-gray-700">Register</button></Link><Link to='/login'><button type="button" className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Login</button></Link>
     </div>
     
     </div>
@@ -31,17 +33,17 @@ export default function Home() {
     <h2 className='text-7xl p-10 pt-20 '>Features</h2>
     <div className='flex flex-wrap justify-evenly'>
 
-          <div className='flex flex-col items-center text-3xl'>
+          <div className='flex flex-col items-center text-3xl p-10'>
           <GroupsIcon sx={{fontSize:"7rem"}}/>
           Connect with Others
         </div>
 
-    <div className='flex flex-col items-center text-3xl'>
+    <div className='flex flex-col items-center text-3xl p-10'>
     <LightbulbIcon sx={{fontSize:"7rem"}}/>
     Get Inspired
     </div>
 
-    <div className='flex flex-col items-center text-3xl'>
+    <div className='flex flex-col items-center text-3xl p-10'>
     <RocketLaunchIcon sx={{fontSize:"7rem"}}/>
     Increase your Reach
     </div>
@@ -51,6 +53,9 @@ export default function Home() {
     </section>
     </div>
     
+    <section className='container m-auto pt-20 mb-20'>
+    
+    </section>
     
     </div>
   )
