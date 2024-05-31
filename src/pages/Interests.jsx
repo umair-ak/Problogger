26014c8aaba2941b290f0d5309eeb03a9ff1033a
Umair <uak1911@gmail.com>
2024-06-01 00:51:04 +0530

    Implemented the selection and unselection of topics feature

--- src/pages/Interests.jsx
+++ src/pages/Interests.jsx
@@ -1,11 +1,29 @@
-import { Toolbar,Card,CardContent,Typography, Grid,Box } from '@mui/material'
+import { Toolbar,Card,CardContent,Typography, Grid,Box, CardActionArea } from '@mui/material'
 import React from 'react'
+import { useState } from 'react';
 
 export default function Interests() {
     
     // make an api call to get all the pages that the user can select as interested
-    const pages = ['react','angular','vue','html','java','javascript','bootstrap','cpp','python'];
-    
+    let p = ['react','angular','vue','html','java','javascript','bootstrap','cpp','python'];
+
+    const [pages, setPages] = useState(p);
+
+    const [selected, setSelected] = useState([]);
+
+    const handlePages = (page) =>{
+        setPages(pages.filter((i)=>  i!=page));
+        setSelected([...selected,page]);
+        console.log("selected"+selected)
+        console.log("pages"+pages)
+      }
+      
+      const handleSelected = (page) =>{
+        setSelected(selected.filter((i)=>  i!=page));
+        setPages([...pages,page])
+        console.log("selected"+selected)
+        console.log("pages"+pages)
+    }
   return (
     <div>
     <Toolbar />
@@ -13,12 +31,26 @@ export default function Interests() {
     <h2 className='text-5xl pb-3 m-5 text-center'>Select your Interests</h2>
     <p className='text-2xl pb-5 m-5 text-center'>select upto 5 pages</p>
     <Grid container spacing={{ xs: 3}}>
+    {selected.map((s)=>{return(
+      <Grid item key={s} xs={2}>
+      <Card variant="outlined" sx={{backgroundColor:"#607d8b"}}>
+        <CardActionArea onClick={()=>handleSelected(s)}>
+        <CardContent>
+        <Typography level="title-md">{s}</Typography>
+        </CardContent>
+        </CardActionArea>
+        </Card>
+      </Grid>
+    )})}
+    
     {pages.map((page)=>{ return(
         <Grid item key={page} xs={2}>
         <Card variant="outlined">
+        <CardActionArea onClick={()=>handlePages(page)}>
         <CardContent>
         <Typography level="title-md">{page}</Typography>
         </CardContent>
+        </CardActionArea>
         </Card>
         </Grid>
     )})}
