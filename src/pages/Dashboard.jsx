ced61448e23b2a05bfba7a7244eed54ca10bc23b
Umair <uak1911@gmail.com>
2024-06-05 20:59:58 +0530

    updated the side menu

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import { useState } from 'react';
 import {ListItem,Divider,List,Toolbar,CssBaseline,Drawer,Box,ListItemButton,ListItemIcon,ListItemText,Grid} from '@mui/material'
 
 
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PostAddIcon from '@mui/icons-material/PostAdd';
+import Person2Icon from '@mui/icons-material/Person2';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import CreateIcon from '@mui/icons-material/Create';
 import BlogPostCard from '../components/BlogPostCard';
 
 export default function Dashboard() {
+
+  // store the interest in a store or in the database and get them using an api call 
+  const [interests,setInterest] = useState(['React','Angular','JavaScript']);
   const drawerWidth = 240;
   return (
     <Box sx={{ display: 'flex' }}>
@@ -22,25 +31,27 @@ export default function Dashboard() {
       <Toolbar />
       <Box sx={{ overflow: 'auto' }}>
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem key={text} disablePadding>
+          {[{text:'Dashboard',icon:<DashboardIcon />},
+           {text:'My posts',icon:<PostAddIcon />},
+           {text:'Profile',icon:<Person2Icon />},
+           {text:'Messages',icon:<MailIcon />}, 
+           {text:'Stats',icon:<BarChartIcon />},
+           {text:'Create post',icon:<CreateIcon />}].map((i) => (
+            <ListItem key={i.text} disablePadding>
               <ListItemButton>
                 <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                {i.icon}
                 </ListItemIcon>
-                <ListItemText primary={text} />
+                <ListItemText primary={i.text} />
               </ListItemButton>
             </ListItem>
           ))}
         </List>
         <Divider />
         <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          {interests.map((text) => (
             <ListItem key={text} disablePadding>
               <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
                 <ListItemText primary={text} />
               </ListItemButton>
             </ListItem>
